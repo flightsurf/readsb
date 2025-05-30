cd775cba7c7506543fedf94d0c256a2be42ea444
Matthias Wirth <matthias.wirth@gmail.com>
2025-05-30 06:56:35 +0000

    increase traceLast default to 96

--- readsb.c
+++ readsb.c
@@ -163,7 +163,7 @@ static void configSetDefaults(void) {
     Modes.netIngest = 0;
     Modes.ingestLimitRate = 5420;
     Modes.json_trace_interval = 20 * 1000;
-    Modes.traceLastMax = 64;
+    Modes.traceLastMax = 96;
     Modes.beforeLandHighRes = 128;
     Modes.state_write_interval = 1 * HOURS;
     Modes.heatmap_current_interval = -15;
