e70292810a2df318576a7e6a6c1efe1de3f8a559
Matthias Wirth <matthias.wirth@gmail.com>
2025-08-17 17:48:26 +0000

    big version jump: 3.15

--- tag.sh
+++ tag.sh
@@ -3,7 +3,7 @@
 set -e
 trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
 
-VERSION="3.14.$(( $(cat version | cut -d'.' -f3) + 1 ))"
+VERSION="3.15.$(( $(cat version | cut -d'.' -f3) + 1 ))"
 echo "$VERSION" > version
 git add version
 
